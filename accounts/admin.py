e443c7c970676a4136db1665a7fb5e00a3b7e377
Vitalii <iamkorniichuk@gmail.com>
2023-04-05 20:08:47 +0300

    Add Employees models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import *
 
+
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('user', 'rating', 'join_date')
     ordering = ('join_date', )
     fields = ('user', )
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'join_date')
+    ordering = ('join_date', )
+    fields = ('customer', )
+
+
+admin.site.register(MyDirector, EmployeeAdmin)
+admin.site.register(MyManager, EmployeeAdmin)
+
+
+@admin.register(Driver)
+class DriverAdmin(EmployeeAdmin):
+    list_display = ('customer', 'rating', 'join_date')
--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,61 @@
-from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 from datetime import datetime
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE,
                                 related_name='customer', primary_key=True)
     join_date = models.DateField(auto_now=True, editable=False)
 
     @property
     def joined_for(self):
         return datetime.now().date() - self.join_date
+
+    @property
+    def rating(self):
+        return ''
+
+    def __str__(self) -> str:
+        return self.user.__str__()
+
+
+class Employee(models.Model):
+    related_customer_name = 'employee'
+    default_job_title = related_customer_name
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE,
+                                    related_name=related_customer_name, primary_key=True)
+    join_date = models.DateField(auto_now=True, editable=False)
+    job_title = models.CharField(max_length=100, default=default_job_title,
+                                 blank=False, null=False)
+
+    @property
+    def joined_for(self):
+        return datetime.now().date() - self.join_date
+
+
+class MyDirector(Employee):
+    related_customer_name = 'director'
+
+    class Meta:
+        verbose_name = 'director'
+        verbose_name_plural = 'directors'
+
+
+class MyManager(Employee):
+    related_customer_name = 'manager'
+    director = models.ForeignKey(MyDirector, on_delete=models.RESTRICT,
+                                 related_name='managers')
+
+    class Meta:
+        verbose_name = 'manager'
+        verbose_name_plural = 'managers'
+
+
+class Driver(Employee):
+    related_customer_name = 'driver'
+    manager = models.ForeignKey(MyManager, on_delete=models.RESTRICT,
+                                related_name='drivers')
 
     @property
     def rating(self):
