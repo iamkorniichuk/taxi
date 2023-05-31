bd0cf69b025d4fbde8040e25eb765e92f8642153
Vitalii <iamkorniichuk@gmail.com>
2023-05-31 14:20:10 +0300

    Set map default

--- orders/admin.py
+++ orders/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from .models import *
 
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('stops', 'price', 'note', 'datetime', 'car_type', 'car_class', 'distance')
-    fields = ('customer', 'stops', 'price', 'note', 'car_type', 'car_class')
+    list_display = ('pk', 'stops', 'price', 'note', 'datetime',
+                    'car_type', 'car_class', 'distance')
+    fields = ('customer', '_stops', 'price', 'note', 'car_type', 'car_class')
     search_fields = ('price', 'car_type', 'car_class')
-    list_filter = (('car_type', admin.ChoicesFieldListFilter), ('car_class', admin.ChoicesFieldListFilter))
-    
+    list_filter = (('car_type', admin.ChoicesFieldListFilter),
+                   ('car_class', admin.ChoicesFieldListFilter))
--- orders/forms.py
+++ orders/forms.py
@@ -6,7 +6,11 @@ from django.contrib.gis import forms
 class MapWidget(forms.OSMWidget):
     def __init__(self):
         super().__init__(attrs={
-            'map_height': 500
+            'map_height': 500,
+            'map_srid': 4326,
+            'default_lat': 50.2547,
+            'default_lon': 28.6587,
+            'default_zoom': 12
         })
 
 
