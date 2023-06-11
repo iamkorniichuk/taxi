ec4e5b40b0ce114f462cd07138708f7289070828
Vitalii <iamkorniichuk@gmail.com>
2023-06-11 09:52:34 +0300

    Add tip form

--- trips/urls.py
+++ trips/urls.py
@@ -8,5 +8,7 @@ app_name = APP_NAME
 urlpatterns = [
     path('<int:pk>/', TripDetailView.as_view(), name='detail'),
     path('', TripListView.as_view(), name='list'),
-    path('end/', trip_end_view, name='end')
+    path('end/', trip_end_view, name='end'),
+    path('tip/', trip_tip_view, name='tip'),
+    path('report/', trip_report_view, name='report'),
 ]
