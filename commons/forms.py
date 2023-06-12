714113497f055d9c03754b4deafaa0151c447806
Vitalii <iamkorniichuk@gmail.com>
2023-06-12 23:13:49 +0300

    Stylize forms' pages

--- commons/forms.py
+++ commons/forms.py
@@ -1,11 +1,14 @@
-from django.forms import Form
+from django.forms import Form, Textarea
 
 
 class BootstrapForm(Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
-            field.widget.attrs.update(
-                {'class': 'form-control', 'placeholder': field.label})
+            attrs = {'class': 'form-control', 'placeholder': field.label}
+            print(type(field.widget))
+            if isinstance(field.widget, Textarea):
+                attrs.update({'rows': '2'})
+            field.widget.attrs.update(attrs)
             field.label = ''
             field.help_text = ''
